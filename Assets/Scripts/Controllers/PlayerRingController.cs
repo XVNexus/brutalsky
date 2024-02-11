1a130825e13fddd4b7c19bb7fa43924405538b99
xv <iberkstresser@protonmail.com>
2024-02-11 00:18:57 -0600

    Add basic particle effects (not yet triggered properly)

--- Assets/Scripts/Controllers/PlayerRingController.cs
+++ Assets/Scripts/Controllers/PlayerRingController.cs
@@ -5,17 +5,14 @@ namespace Controllers
 {
     public class PlayerRingController : MonoBehaviour
     {
-        public float idleSpinSpeed = 30f;
-        public float chargeSpinSpeed = 180f;
         public SpriteRenderer cSpriteRenderer;
         public SpriteMask cSpriteMask;
 
         private PlayerHealthController cPlayerHealthController;
         private PlayerMovementController cPlayerMovementController;
-        private float ringHue = 0f;
         private float ringAlpha = 0f;
-        private float ringSpinSpeed = 0f;
-        private float ringMask = 0f;
+        private float ringThickness = 0f;
+        private float ringSpin = 0f;
 
         private void Start()
         {
@@ -25,41 +22,37 @@ namespace Controllers
 
         private void FixedUpdate()
         {
-            var targetRingHue = (cPlayerHealthController.health / cPlayerHealthController.maxHealth) / 3f;
             float targetRingAlpha;
-            float targetRingSpinSpeed;
-            float targetRingMask;
+            float targetRingThickness = cPlayerHealthController.health / cPlayerHealthController.maxHealth;
+            float targetRingSpin;
             if (cPlayerMovementController.boostCharge > 0f)
             {
-                targetRingAlpha = .25f + cPlayerMovementController.boostCharge * .75f / 3f;
-                targetRingSpinSpeed = (Mathf.Pow(cPlayerMovementController.boostCharge, 2f) + 2f) * chargeSpinSpeed + 90f;
-                targetRingMask = .5f + cPlayerMovementController.boostCharge * .5f / 3f;
+                targetRingAlpha = .25f + cPlayerMovementController.boostCharge * .25f;
+                targetRingSpin = (Mathf.Pow(cPlayerMovementController.boostCharge, 2f) + 2f) * 180f + 90f;
             }
             else if (cPlayerMovementController.boostCooldown > 0f)
             {
-                targetRingAlpha = .25f;
-                targetRingSpinSpeed = 0f;
-                targetRingMask = .25f;
+                targetRingAlpha = .05f;
+                targetRingSpin = 10f;
             }
             else
             {
-                targetRingAlpha = .4f;
-                targetRingSpinSpeed = idleSpinSpeed;
-                targetRingMask = .5f;
+                targetRingAlpha = .25f;
+                targetRingSpin = 40f;
             }
 
-            ringHue = MathfExt.MoveTo(ringHue, targetRingHue, Time.fixedDeltaTime);
             ringAlpha = MathfExt.MoveTo(ringAlpha, targetRingAlpha, Time.fixedDeltaTime);
-            ringSpinSpeed = MathfExt.MoveTo(ringSpinSpeed, targetRingSpinSpeed, 1440f * Time.fixedDeltaTime);
-            ringMask = MathfExt.MoveTo(ringMask, targetRingMask, Time.fixedDeltaTime);
+            ringThickness = MathfExt.MoveTo(ringThickness, targetRingThickness, Time.fixedDeltaTime);
+            ringSpin = MathfExt.MoveTo(ringSpin, targetRingSpin, 1440f * Time.fixedDeltaTime);
 
-            var ringColorRgb = Color.HSVToRGB(ringHue, .75f, 1f);
-            cSpriteRenderer.color = new Color(ringColorRgb.r, ringColorRgb.g, ringColorRgb.b, ringAlpha);
+            var ringColor = cSpriteRenderer.color;
+            ringColor.a = ringAlpha;
+            cSpriteRenderer.color = ringColor;
             var ringTransform = cSpriteRenderer.transform;
             var ringAngles = ringTransform.localEulerAngles;
-            ringAngles.z -= ringSpinSpeed * Time.fixedDeltaTime;
+            ringAngles.z -= ringSpin * Time.fixedDeltaTime;
             ringTransform.localEulerAngles = ringAngles;
-            var ringMaskScale = 2f - ringMask * .8f;
+            var ringMaskScale = 2f - ringThickness * .8f;
             cSpriteMask.transform.localScale = new Vector3(ringMaskScale, ringMaskScale, 1f);
         }
     }
