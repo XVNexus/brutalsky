1621050d8aef0a0a6ae97c5fc416a0b3a939f298
xv <58712159+XVNexus@users.noreply.github.com>
2024-05-06 02:27:52 -0500

    Refactor map generator
    Move map generator code to standalone class and move general map config to shared constants

--- Assets/Scripts/Core/GameManager.cs
+++ Assets/Scripts/Core/GameManager.cs
@@ -1,14 +1,11 @@
 using System.Linq;
 using Brutalsky;
-using Brutalsky.Map;
 using Brutalsky.Object;
 using Controllers;
 using Controllers.Base;
 using Controllers.Player;
 using UnityEngine;
-using Utils.Constants;
-using Utils.Object;
-using Utils.Shape;
+using Utils.Map;
 
 namespace Core
 {
@@ -30,12 +27,20 @@ namespace Core
         protected override void OnLoad()
         {
             MapSystem.ResaveBuiltinMaps(new[] { "Brutalsky", "Doomring", "Tossup", "Void" });
-            GenerateDefaultMaps();
+
+            var shapeNames = new[] { "Platform", "Box", "Cage", "Tunnel" };
+            var shapes = new[] { 0b1000, 0b1011, 0b1111, 0b1100 };
+            var sizeNames = new[] { "Small", "Medium", "Large" };
+            var sizes = new[] { new Vector2(20f, 10f), new Vector2(40f, 20f), new Vector2(80f, 40f) };
+            for (var i = 0; i < shapes.Length; i++) for (var j = 0; j < sizes.Length; j++)
+            {
+                MapSystem.SaveMap(MapGenerator.Box($"{sizeNames[j]} {shapeNames[i]}", shapes[i], sizes[j]));
+            }
         }
 
         protected override void OnLink()
         {
-            StartGame(MapSystem.GenerateId("Void", "Xveon"), new[]
+            StartGame(MapSystem.GenerateId("Brutalsky", "Xveon"), new[]
             {
                 new BsPlayer("Player 1", 100f, new Color(1f, .5f, 0f)),
                 new BsPlayer("Player 2", 100f, new Color(0f, .5f, 1f), true)
@@ -111,105 +116,6 @@ namespace Core
             if (livingPlayers.Count != 1) return;
             Debug.Log($"{livingPlayers[0].Name} wins!");
             Invoke(nameof(RestartRound), 3f);
-        }
-
-        // TODO: TEMPORARY FUNCTIONS
-        private static void GenerateDefaultMaps()
-        {
-            var shapes = new[] { 0b1000, 0b1011, 0b1111, 0b1100 };
-            var shapeNames = new[] { "Platform", "Box", "Cage", "Tunnel" };
-            var sizes = new[] { 20f, 40f, 80f };
-            var sizeNames = new[] { "Small", "Medium", "Large" };
-            for (var i = 0; i < shapes.Length; i++) for (var j = 0; j < sizes.Length; j++)
-            {
-                GenerateBoxMap($"{sizeNames[j]} {shapeNames[i]}", shapes[i], sizes[j]);
-            }
-        }
-
-        private static void GenerateBoxMap(string title, int shape, float size)
-        {
-            var bottom = (shape & 0b1000) > 0;
-            var top = (shape & 0b0100) > 0;
-            var left = (shape & 0b0010) > 0;
-            var right = (shape & 0b0001) > 0;
-            var map = new BsMap(title, "Brutalsky")
-            {
-                PlayArea = new Vector2(size, size * .5f),
-                BackgroundColor = new Color(.025f, .25f, .025f),
-                LightingColor = new Color(.1f, 1f, .1f, .8f),
-                GravityDirection = Direction.Down,
-                GravityStrength = 20f,
-                PlayerHealth = 100f
-            };
-            map.AddSpawn(new BsSpawn(new Vector2(-3f, -size * .25f + 1.5f), 0));
-            map.AddSpawn(new BsSpawn(new Vector2(-1f, -size * .25f + 1.5f), 0));
-            map.AddSpawn(new BsSpawn(new Vector2(1f, -size * .25f + 1.5f), 0));
-            map.AddSpawn(new BsSpawn(new Vector2(3f, -size * .25f + 1.5f), 0));
-            if (bottom) map.AddObject(new BsShape
-            (
-                "wall-bottom",
-                new ObjectTransform(0f, -size * .25f + .5f),
-                ObjectLayer.Midground,
-                true,
-                Form.Rectangle(size, 1f)
-            ));
-            if (top) map.AddObject(new BsShape
-            (
-                "wall-top",
-                new ObjectTransform(0f, size * .25f - .5f),
-                ObjectLayer.Midground,
-                true,
-                Form.Rectangle(size, 1f)
-            ));
-            if (left) map.AddObject(new BsShape
-            (
-                "wall-left",
-                new ObjectTransform(-size * .5f + .5f, 0f),
-                ObjectLayer.Midground,
-                true,
-                Form.Rectangle(1f, size * .5f)
-            ));
-            if (right) map.AddObject(new BsShape
-            (
-                "wall-right",
-                new ObjectTransform(size * .5f - .5f, 0f),
-                ObjectLayer.Midground,
-                true,
-                Form.Rectangle(1f, size * .5f)
-            ));
-            if (top && left) map.AddObject(new BsShape
-            (
-                "corner-tl",
-                new ObjectTransform(-size * .5f + 1f, size * .25f - 1f),
-                ObjectLayer.Midground,
-                true,
-                Form.Vector(new[] { 0f, 0f, 0f, 3f, 0f, 1f, 0f, 0f, 0f, -3f })
-            ));
-            if (top && right) map.AddObject(new BsShape
-            (
-                "corner-tr",
-                new ObjectTransform(size * .5f - 1f, size * .25f - 1f),
-                ObjectLayer.Midground,
-                true,
-                Form.Vector(new[] { 0f, 0f, 0f, -3f, 0f, 1f, 0f, 0f, 0f, -3f })
-            ));
-            if (bottom && left) map.AddObject(new BsShape
-            (
-                "corner-bl",
-                new ObjectTransform(-size * .5f + 1f, -size * .25f + 1f),
-                ObjectLayer.Midground,
-                true,
-                Form.Vector(new[] { 0f, 0f, 0f, 3f, 0f, 1f, 0f, 0f, 0f, 3f })
-            ));
-            if (bottom && right) map.AddObject(new BsShape
-            (
-                "corner-br",
-                new ObjectTransform(size * .5f - 1f, -size * .25f + 1f),
-                ObjectLayer.Midground,
-                true,
-                Form.Vector(new[] { 0f, 0f, 0f, -3f, 0f, 1f, 0f, 0f, 0f, 3f })
-            ));
-            MapSystem.SaveMap(map);
         }
     }
 }
---
+++ Assets/Scripts/Utils/Map/MapGenerator.cs
@@ -0,0 +1,121 @@
+using Brutalsky;
+using Brutalsky.Map;
+using Brutalsky.Object;
+using UnityEngine;
+using Utils.Constants;
+using Utils.Ext;
+using Utils.Object;
+using Utils.Shape;
+
+namespace Utils.Map
+{
+    public static class MapGenerator
+    {
+        public static readonly Color MapBackground = new(.3f, .3f, .3f);
+        public static readonly Color MapLighting = new(1f, 1f, 1f, .8f);
+        public static readonly ShapeMaterial ObjectMaterial = ShapeMaterial.Stone;
+        public static readonly Color ObjectColor = ColorExt.Stone;
+
+        public static BsMap Box(string title, int shape, Vector2 size)
+        {
+            var bottom = (shape & 0b1000) > 0;
+            var top = (shape & 0b0100) > 0;
+            var left = (shape & 0b0010) > 0;
+            var right = (shape & 0b0001) > 0;
+            var result = new BsMap(title, "Brutalsky")
+            {
+                PlayArea = size,
+                BackgroundColor = MapBackground,
+                LightingColor = MapLighting,
+                GravityDirection = Direction.Down,
+                GravityStrength = 20f,
+                PlayerHealth = 100f
+            };
+            result.AddSpawn(new BsSpawn(new Vector2(-3f, -size.y * .5f + 1.5f), 0));
+            result.AddSpawn(new BsSpawn(new Vector2(-1f, -size.y * .5f + 1.5f), 0));
+            result.AddSpawn(new BsSpawn(new Vector2(1f, -size.y * .5f + 1.5f), 0));
+            result.AddSpawn(new BsSpawn(new Vector2(3f, -size.y * .5f + 1.5f), 0));
+            if (bottom) result.AddObject(new BsShape
+            (
+                "wall-bottom",
+                new ObjectTransform(0f, size.y * -.5f + .5f),
+                ObjectLayer.Midground,
+                true,
+                Form.Rectangle(size.x, 1f),
+                ObjectMaterial,
+                ObjectColor
+            ));
+            if (top) result.AddObject(new BsShape
+            (
+                "wall-top",
+                new ObjectTransform(0f, size.y * .5f - .5f),
+                ObjectLayer.Midground,
+                true,
+                Form.Rectangle(size.x, 1f),
+                ObjectMaterial,
+                ObjectColor
+            ));
+            if (left) result.AddObject(new BsShape
+            (
+                "wall-left",
+                new ObjectTransform(size.x * -.5f + .5f, 0f),
+                ObjectLayer.Midground,
+                true,
+                Form.Rectangle(1f, size.y),
+                ObjectMaterial,
+                ObjectColor
+            ));
+            if (right) result.AddObject(new BsShape
+            (
+                "wall-right",
+                new ObjectTransform(size.x * .5f - .5f, 0f),
+                ObjectLayer.Midground,
+                true,
+                Form.Rectangle(1f, size.y),
+                ObjectMaterial,
+                ObjectColor
+            ));
+            if (top && left) result.AddObject(new BsShape
+            (
+                "corner-tl",
+                new ObjectTransform(size.x * -.5f + 1f, size.y * .5f - 1f),
+                ObjectLayer.Midground,
+                true,
+                Form.Vector(new[] { 0f, 0f, 0f, 3f, 0f, 1f, 0f, 0f, 0f, -3f }),
+                ObjectMaterial,
+                ObjectColor
+            ));
+            if (top && right) result.AddObject(new BsShape
+            (
+                "corner-tr",
+                new ObjectTransform(size.x * .5f - 1f, size.y * .5f - 1f),
+                ObjectLayer.Midground,
+                true,
+                Form.Vector(new[] { 0f, 0f, 0f, -3f, 0f, 1f, 0f, 0f, 0f, -3f }),
+                ObjectMaterial,
+                ObjectColor
+            ));
+            if (bottom && left) result.AddObject(new BsShape
+            (
+                "corner-bl",
+                new ObjectTransform(size.x * -.5f + 1f, size.y * -.5f + 1f),
+                ObjectLayer.Midground,
+                true,
+                Form.Vector(new[] { 0f, 0f, 0f, 3f, 0f, 1f, 0f, 0f, 0f, 3f }),
+                ObjectMaterial,
+                ObjectColor
+            ));
+            if (bottom && right) result.AddObject(new BsShape
+            (
+                "corner-br",
+                new ObjectTransform(size.x * .5f - 1f, size.y * -.5f + 1f),
+                ObjectLayer.Midground,
+                true,
+                Form.Vector(new[] { 0f, 0f, 0f, -3f, 0f, 1f, 0f, 0f, 0f, 3f }),
+                ObjectMaterial,
+                ObjectColor
+            ));
+            return result;
+        }
+    }
+}
